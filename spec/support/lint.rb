5fe3426cc7cfc0e3825a1f0bc888b8a424d9f60c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-08-22 11:22:43 -0300

    Fix tests
    Fix tests by making sure the argument was one hash instead of keyword
    arguments.

--- spec/support/lint.rb
+++ spec/support/lint.rb
@@ -1,18 +1,18 @@
 shared_examples :lint do
   let(:interactor) { Class.new.send(:include, described_class) }
 
-  describe ".call" do
+  describe '.call' do
     let(:context) { double(:context) }
     let(:instance) { double(:instance, context: context) }
 
-    it "calls an instance with the given context" do
-      expect(interactor).to receive(:new).once.with(foo: "bar") { instance }
+    it 'calls an instance with the given context' do
+      expect(interactor).to receive(:new).once.with({ foo: 'bar' }) { instance }
       expect(instance).to receive(:run).once.with(no_args)
 
-      expect(interactor.call(foo: "bar")).to eq(context)
+      expect(interactor.call(foo: 'bar')).to eq(context)
     end
 
-    it "provides a blank context if none is given" do
+    it 'provides a blank context if none is given' do
       expect(interactor).to receive(:new).once.with({}) { instance }
       expect(instance).to receive(:run).once.with(no_args)
 
@@ -20,18 +20,18 @@ shared_examples :lint do
     end
   end
 
-  describe ".call!" do
+  describe '.call!' do
     let(:context) { double(:context) }
     let(:instance) { double(:instance, context: context) }
 
-    it "calls an instance with the given context" do
-      expect(interactor).to receive(:new).once.with(foo: "bar") { instance }
+    it 'calls an instance with the given context' do
+      expect(interactor).to receive(:new).once.with({ foo: 'bar' }) { instance }
       expect(instance).to receive(:run!).once.with(no_args)
 
-      expect(interactor.call!(foo: "bar")).to eq(context)
+      expect(interactor.call!(foo: 'bar')).to eq(context)
     end
 
-    it "provides a blank context if none is given" do
+    it 'provides a blank context if none is given' do
       expect(interactor).to receive(:new).once.with({}) { instance }
       expect(instance).to receive(:run!).once.with(no_args)
 
@@ -39,19 +39,19 @@ shared_examples :lint do
     end
   end
 
-  describe ".new" do
+  describe '.new' do
     let(:context) { double(:context) }
 
-    it "initializes a context" do
-      expect(Interactor::Context).to receive(:build).once.with(foo: "bar") { context }
+    it 'initializes a context' do
+      expect(Interactor::Context).to receive(:build).once.with({ foo: 'bar' }) { context }
 
-      instance = interactor.new(foo: "bar")
+      instance = interactor.new(foo: 'bar')
 
       expect(instance).to be_a(interactor)
       expect(instance.context).to eq(context)
     end
 
-    it "initializes a blank context if none is given" do
+    it 'initializes a blank context if none is given' do
       expect(Interactor::Context).to receive(:build).once.with({}) { context }
 
       instance = interactor.new
@@ -61,80 +61,80 @@ shared_examples :lint do
     end
   end
 
-  describe "#run" do
+  describe '#run' do
     let(:instance) { interactor.new }
 
-    it "runs the interactor" do
+    it 'runs the interactor' do
       expect(instance).to receive(:run!).once.with(no_args)
 
       instance.run
     end
 
-    it "rescues failure with the same context" do
+    it 'rescues failure with the same context' do
       expect(instance).to receive(:run!).and_raise(Interactor::Failure.new(instance.context))
 
-      expect {
+      expect do
         instance.run
-      }.not_to raise_error
+      end.not_to raise_error
     end
 
-    it "raises other failures" do
+    it 'raises other failures' do
       expect(instance).to receive(:run!).and_raise(Interactor::Failure.new(Interactor::Context.new))
 
-      expect {
+      expect do
         instance.run
-      }.to raise_error(Interactor::Failure)
+      end.to raise_error(Interactor::Failure)
     end
 
-    it "raises other errors" do
-      expect(instance).to receive(:run!).and_raise("foo")
+    it 'raises other errors' do
+      expect(instance).to receive(:run!).and_raise('foo')
 
-      expect {
+      expect do
         instance.run
-      }.to raise_error("foo")
+      end.to raise_error('foo')
     end
   end
 
-  describe "#run!" do
+  describe '#run!' do
     let(:instance) { interactor.new }
 
-    it "calls the interactor" do
+    it 'calls the interactor' do
       expect(instance).to receive(:call).once.with(no_args)
 
       instance.run!
     end
 
-    it "raises failure" do
+    it 'raises failure' do
       expect(instance).to receive(:run!).and_raise(Interactor::Failure)
 
-      expect {
+      expect do
         instance.run!
-      }.to raise_error(Interactor::Failure)
+      end.to raise_error(Interactor::Failure)
     end
 
-    it "raises other errors" do
-      expect(instance).to receive(:run!).and_raise("foo")
+    it 'raises other errors' do
+      expect(instance).to receive(:run!).and_raise('foo')
 
-      expect {
+      expect do
         instance.run
-      }.to raise_error("foo")
+      end.to raise_error('foo')
     end
   end
 
-  describe "#call" do
+  describe '#call' do
     let(:instance) { interactor.new }
 
-    it "exists" do
+    it 'exists' do
       expect(instance).to respond_to(:call)
       expect { instance.call }.not_to raise_error
       expect { instance.method(:call) }.not_to raise_error
     end
   end
 
-  describe "#rollback" do
+  describe '#rollback' do
     let(:instance) { interactor.new }
 
-    it "exists" do
+    it 'exists' do
       expect(instance).to respond_to(:rollback)
       expect { instance.rollback }.not_to raise_error
       expect { instance.method(:rollback) }.not_to raise_error
