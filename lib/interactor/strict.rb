c06d807988de4ca1e7bafa2e1bc527d6fa033bc6
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-10-25 10:50:32 -0300

    Implement Interactor::Strict

---
+++ lib/interactor/strict.rb
@@ -0,0 +1,38 @@
+module Interactor
+  module Strict
+    def self.included(base)
+      base.class_eval do
+        extend ClassMethods
+        include Hooks
+
+        # Public: Gets the Interactor::Context of the Interactor instance.
+        attr_reader :context
+      end
+    end
+
+    def initialize(*args, **kargs)
+      unless args.empty?
+        raise ArgumentError, "wrong number of arguments (given #{args.size}, expected 0)"
+      end
+
+      @kargs = kargs
+      @context = Context.build(*args)
+    end
+
+    def run!
+      with_hooks do
+        call(**@kargs)
+        context.called!(self)
+      end
+    rescue
+      context.rollback!
+      raise
+    end
+
+    module ClassMethods
+      def call(*args, **kargs)
+        new(*args, **kargs).tap(&:run).context
+      end
+    end
+  end
+end
